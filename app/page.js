588b854a8c6d5c4d19283abff33d15f53e3d1408
unknown <mwarishijoseph@gmail.com>
2024-12-23 13:24:18 +0300

    second commit

--- app/page.js
+++ app/page.js
@@ -1,101 +1,87 @@
-import Image from "next/image";
+"use client"
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [countries, setcountries] = useState([])
+  const [states, setStates] = useState([])
+  const [cities, setCities] = useState([])
+  const [stateStatus, setStateStatus] = useState(true)
+  const [cityStatus, setCityStatus] = useState(true)
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  var country_api_url = "https://api-dev.autoby24.ch/api/core/country?limit=all"
+  const getcountries = async () => {
+    const resp = await fetch(country_api_url);
+    if (!resp.ok) {
+      console.log("error fetching countries")
+    } else {
+      const data = await resp.json()
+      console.log(data)
+      setcountries(data["results"])
+    }
+  }
+  useEffect(() => {
+    getcountries()
+  }, []);
+
+  const getStates = async (e) => {
+    setStates([]);
+    // setSelectedCountry()
+    const fstate_url = `https://api-dev.autoby24.ch/api/core/state?country=${e.target.value}&limit=all`;
+    const resp = await fetch(fstate_url, {
+      cache: 'no-store'
+    })
+    if (!resp.ok) {
+      console.log("Failed fetching countries....")
+    } else {
+      const data = await resp.json()
+      setStates(data["results"])
+    }
+    setStateStatus(false);
+  }
+
+  const getCities = async (e) => {
+    const city_url = `https://api-dev.autoby24.ch/api/core/city?state=${e.target.value}&limit=all`
+    const resp = await fetch(city_url, {
+      cache: 'no-store'
+    });
+    if (!resp.ok) {
+      console.log("ERror fetching cities....")
+    } else {
+      const data = await resp.json()
+      setCities(data["results"]);
+      setCityStatus(false);
+    }
+  }
+
+  return (
+    <div className="grid grid-rows-[10px_1fr_10px] items-center justify-items-center min-h-screen p-8 pb-10 gap-1 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <h1>Autoby Countries API Intergration</h1>
+      <div className="flex justify-between space-x-6">
+        <div className="mb-0">
+          <select className="select select-primary w-full max-w-xs" onChange={(e) => getStates(e)}>
+            <option disabled selected>Select country</option>
+            {countries.map((country) => {
+              return <option key={country.uuid} value={country.name} >{country.name}</option>
+            })}
+          </select>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className="mb-o">
+          <select className="select select-primary w-full max-w-xs" disabled={stateStatus} onChange={(e) => getCities(e)}>
+            <option disabled selected>Select State</option>
+            {states.map((state) => {
+              return <option key={state.uuid} value={state.name} >{state.name}</option>
+            })}
+          </select>
+        </div>
+        <div className="mb-o">
+          <select className="select select-primary w-full max-w-xs" disabled={cityStatus}>
+            <option disabled selected>Select City</option>
+            {cities.map((city) => {
+              return <option key={city.uuid} value={city.name} >{city.name}</option>
+            })}
+          </select>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ commit.bat
@@ -0,0 +1,7 @@
+echo "# autopy24-country-api" >> README.md
+call git init
+git add .
+call git commit -m "second commit"
+call git branch -M main
+call git remote add origin https://github.com/josanta/autopy24-country-api.git
+call git push -u origin main
